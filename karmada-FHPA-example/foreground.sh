09f3ee40589ccc436a4012742c2d60ee42d1b281
Krishiv-Mahajan <mahajankrishiv10@gmail.com>
2026-03-29 19:40:10 +0530

    fixing the setup 2
    Signed-off-by: Krishiv-Mahajan <mahajankrishiv10@gmail.com>

--- karmada-FHPA-example/foreground.sh
+++ karmada-FHPA-example/foreground.sh
@@ -1,21 +1,87 @@
 #!/usr/bin/env bash
 
-# Source common setup functions and variables.
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if [ -f "${SCRIPT_DIR}/../common-setup.sh" ]; then
-  COMMON_SETUP="${SCRIPT_DIR}/../common-setup.sh"
-elif [ -f "${SCRIPT_DIR}/common-setup.sh" ]; then
-  COMMON_SETUP="${SCRIPT_DIR}/common-setup.sh"
-elif [ -f "${PWD}/common-setup.sh" ]; then
-  COMMON_SETUP="${PWD}/common-setup.sh"
-else
-  echo "common-setup.sh not found. Checked:"
-  echo "  ${SCRIPT_DIR}/../common-setup.sh"
-  echo "  ${SCRIPT_DIR}/common-setup.sh"
-  echo "  ${PWD}/common-setup.sh"
-  exit 1
-fi
-source "${COMMON_SETUP}"
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Common variable definitions
+kind_version=v0.17.0
+host_cluster_ip=172.30.1.2
+member_cluster_ip=172.30.2.2
+local_ip=127.0.0.1
+KUBECONFIG_PATH=${KUBECONFIG_PATH:-"${HOME}/.kube"}
+
+function installKind() {
+    cat << EOF > installKind.sh
+    wget https://github.com/kubernetes-sigs/kind/releases/download/${kind_version}/kind-linux-amd64
+    chmod +x kind-linux-amd64
+    sudo mv kind-linux-amd64 /usr/local/bin/kind
+EOF
+}
+
+function createCluster() {
+    cat << EOF > createCluster.sh
+    kind create cluster --name=member1 --config=cluster1.yaml
+    mv \$HOME/.kube/config ~/config-member1
+    kind create cluster --name=member2 --config=cluster2.yaml
+    mv \$HOME/.kube/config config-member2
+    KUBECONFIG=~/config-member1:~/config-member2 kubectl config view --merge --flatten >> ${KUBECONFIG_PATH}/config
+    sed -i "s/${local_ip}/${member_cluster_ip}/g" config-member1
+    scp -o StrictHostKeyChecking=no config-member1 root@${host_cluster_ip}:\$HOME/.kube/config-member1
+    sed -i "s/${local_ip}/${member_cluster_ip}/g" config-member2
+    scp -o StrictHostKeyChecking=no config-member2 root@${host_cluster_ip}:\$HOME/.kube/config-member2
+EOF
+}
+
+function cluster1Config() {
+    cat << EOF > cluster1.yaml
+    kind: Cluster
+    apiVersion: kind.x-k8s.io/v1alpha4
+    networking:
+      apiServerAddress: "${member_cluster_ip}"
+      apiServerPort: 6443
+EOF
+}
+
+function cluster2Config() {
+    cat << EOF > cluster2.yaml
+    kind: Cluster
+    apiVersion: kind.x-k8s.io/v1alpha4
+    networking:
+      apiServerAddress: "${member_cluster_ip}"
+      apiServerPort: 6444
+EOF
+}
+
+function copyConfigFilesToNode() {
+    scp -o StrictHostKeyChecking=no \
+        installKind.sh \
+        createCluster.sh \
+        cluster1.yaml \
+        cluster2.yaml \
+        root@${member_cluster_ip}:~
+}
+
+function setupKubectl() {
+    kubectl delete node node01
+    kubectl taint node controlplane node-role.kubernetes.io/control-plane:NoSchedule-
+}
+
+function createMemberClusters() {
+    ssh -o StrictHostKeyChecking=no root@${member_cluster_ip} "bash ~/installKind.sh"
+    ssh -o StrictHostKeyChecking=no root@${member_cluster_ip} "bash ~/createCluster.sh"
+}
+
+function installKarmadactl() {
+    curl -s https://raw.githubusercontent.com/karmada-io/karmada/master/hack/install-cli.sh | sudo bash
+}
+
+function joinMemberClusters() {
+    MEMBER_CLUSTER_NAME=kind-member1
+    karmadactl --kubeconfig /etc/karmada/karmada-apiserver.config join ${MEMBER_CLUSTER_NAME} --cluster-kubeconfig=$HOME/.kube/config-member1 --cluster-context=kind-member1
+    MEMBER_CLUSTER_NAME=kind-member2
+    karmadactl --kubeconfig /etc/karmada/karmada-apiserver.config join ${MEMBER_CLUSTER_NAME} --cluster-kubeconfig=$HOME/.kube/config-member2 --cluster-context=kind-member2
+}
 
 # ---------------------------------------------------------------------------
 # Manifest generators
